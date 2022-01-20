dd94040733eefefd86f58cbbf3908b295a0b47e9
Rodrigo Alonso <rodrigo.alonso.o@gmail.com>
2022-01-20 08:18:56 -0300

    acceso: revisar casos borde para soluciones adhoc

--- acceso/attic/generate.py
+++ acceso/attic/generate.py
@@ -11,22 +11,25 @@ import sys
 #
 # 4. Valor de F
 #
-# 5. Probabilidad de poner vereda (entre 0 y 1), 7 hace override
+# 5. Probabilidad de poner vereda horizontal (entre 0 y 1), 8 hace override
 #
-# 6. Lugar de partida:
+# 6. Probabilidad de poner vereda vertical (entre 0 y 1), 8 hace override
+#
+# 7. Lugar de partida:
 #   1: parte en 1, 1
 #   2: parte en 1, N
 #   3: parte en M, 1 
 #   4: parte en M, N
 #   etc, parte en posición random
 #
-# 7. Veredas en lugar de partida:
+# 8. Veredas en lugar de partida:
 #   0: no hay
 #   1: hay horizontal
 #   2: hay vertical
 #   3: hay ambas
 #
-# 8. Puntos de interés:
+# 9. Puntos de interés:
+#   -4: en cada cuadra c/veredas
 #   -3: en cada cuadra de la 1ra y última calle vertical y horizontal c/veredas
 #   -2: en cada cuadra de la 1ra y última calle vertical c/veredas
 #   -1: en cada cuadra de la 1ra y última calle horizontal c/veredas
@@ -38,19 +41,21 @@ random.seed(str(sys.argv[1:]))
 m = int(sys.argv[2])
 n = int(sys.argv[3])
 f = int(sys.argv[4])
-r = float(sys.argv[5])
-s = int(sys.argv[6])
-v = int(sys.argv[7])
-t = int(sys.argv[8])
+pm = float(sys.argv[5])
+pn = float(sys.argv[6])
+s = int(sys.argv[7])
+v = int(sys.argv[8])
+t = int(sys.argv[9])
 
 # Verificaciones
 
-assert 0 < min(m, n)
-assert max(m, n) <= 100
+assert 0 < m <= 100
+assert 0 < n <= 100
 assert 1 <= f <= 500
-assert 0 <= r <= 1
+assert 0 <= pm <= 1
+assert 0 <= pn <= 1
 assert 0 <= v <= 3
-assert -3 <= t <= 1000
+assert -4 <= t <= 1000
 
 # Cálculo del output
 
@@ -64,22 +69,31 @@ for i in range(m):
     if i == x - 1:
         horizontal[i] = v & 1
     else:
-        horizontal[i] = int(random.random() < r)
+        horizontal[i] = int(random.random() < pm)
 
 vertical = [0] * n
 for i in range(n):
     if i == y - 1:
         vertical[i] = (v & 2) >> 1
     else:
-        vertical[i] = int(random.random() < r)
+        vertical[i] = int(random.random() < pn)
 
 pdi = []
-if -t & 1:
+if t == -4:
+    calles = [idx for idx, val in enumerate(horizontal) if val == 1]
+    for i in calles:
+        for j in range(n - 1):
+            pdi.append((i + 1, j + 1, 0))
+    calles = [idx for idx, val in enumerate(vertical) if val == 1]
+    for i in range(m - 1):
+        for j in calles:
+            pdi.append((i + 1, j + 1, 0))
+if t in [-1. -3]:
     calles = [idx for idx, val in enumerate(horizontal) if val == 1]
     if calles:
         for j in range(n - 1):
             pdi.extend(((calles[0] + 1, j + 1, 0), (calles[-1] + 1, j + 1, 0)))
-if -t & 2:
+if t in [-2, -3]:
     calles = [idx for idx, val in enumerate(vertical) if val == 1]
     if calles:
         for i in range(m - 1):
